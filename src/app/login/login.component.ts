8d9061ad9c87f5e082327f1e548f6ed09183cd6d
Superblock Minecraft <mattvey0311@gmail.com>
2022-11-17 13:33:34 +0300

    zachita

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username: string = ''
+  password: string = ''
+  email: string = ''
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  login(): void{
+    let user_obj = {
+      username: this.username,
+      password: this.password,
+      email: this.email
+    }
+    let user_str: string = JSON.stringify(user_obj);
+    localStorage.setItem('userinfo', user_str);
+  }
+
 
 }
